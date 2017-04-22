1ddda3098c94f84f23a2683416cf23baf984b963
NekuSoul <Git@NekuSoul.de>
2017-04-22 14:53:41 +0200

    Initialized project

---
+++ Assets/Game/Scripts/LevelScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Game.Scripts
+{
+    public class LevelScript : MonoBehaviour
+    {
+        // Use this for initialization
+        public void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        public void Update()
+        {
+
+        }
+    }
+}
